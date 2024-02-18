54fbc33cd6a7c8f65443d6d28ce6cd59e2eb1f11
jeugeniojunior <jeugenio.junior@gmail.com>
2024-02-18 17:54:53 -0300

    TESTES do modulo recondutoramento

--- auxiliar.py
+++ auxiliar.py
@@ -2,8 +2,9 @@ import pyautogui
 import time
 
 time.sleep(2)
-#posicao = pyautogui.position()
-#print(posicao)
+posicao = pyautogui.position()
+
+print(posicao)
 
 pyautogui.moveTo(80, 280)
 pyautogui.click(80, 280)
